f9aa69fbc37d472dc5190e5b1a37b22abf4f6433
tianlang.zhou <1490784129@qq.com>
2023-05-24 00:15:26 +0800

    v1.0.1购买系统初版

--- flash-core/src/main/java/cn/enilu/flash/service/system/BuyService.java
+++ flash-core/src/main/java/cn/enilu/flash/service/system/BuyService.java
@@ -32,23 +32,49 @@ public class BuyService extends BaseService<Buy, Long, BuyRepository> {
         String buyId = buyData.getBuyId();
         String buyName = buyData.getBuyName();
         //识别
-        //识别一：7+8+9+10 各10块
         String data = buyData.getBuyData();
-        String[] splitBlack = data.split(" ");
-        if(splitBlack != null && splitBlack.length >= 2){
-            if(StringUtils.isNoneBlank(splitBlack[0])) {
-                String[] splitSum = splitBlack[0].split("\\+");
-                int buyCount = 0;
-                if (StringUtils.isNoneBlank(splitBlack[1]) && splitBlack[1].length() >= 3) {
-                    buyCount = Integer.valueOf(splitBlack[1].substring(1,2));
-                }
-                for(int i=0;i<splitSum.length;i++){
-                    Buy buy = new Buy();
-                    buy.setBuyId(buyId);
-                    buy.setBuyName(buyName);
-                    buy.setBuyCode(Integer.valueOf(splitSum[i]));
-                    buy.setBuyCount(buyCount);
-                    buyList.add(buy);
+        String[] splitAll = data.split("\n");
+        if(splitAll != null && splitAll.length >= 1){
+            for(int i=0;i<splitAll.length;i++){
+                String splitLine= splitAll[i];
+                if(StringUtils.isNoneBlank(splitLine) && splitLine.contains("各")) {
+                    //识别一：7+8+9+10 各10块
+                    String[] splitBlack = splitLine.split(" ");
+                    if(splitBlack != null && splitBlack.length >= 2){
+                        if(StringUtils.isNoneBlank(splitBlack[0])) {
+                            String[] splitSum = splitBlack[0].split("\\+");
+                            int buyCount = 0;
+                            if (StringUtils.isNoneBlank(splitBlack[1]) && splitBlack[1].length() >= 2) {
+                                buyCount = Integer.valueOf(splitBlack[1].substring(1,splitBlack[1].length()-1));
+                            }
+                            for(int j=0;j<splitSum.length;j++){
+                                Buy buy = new Buy();
+                                buy.setBuyId(buyId);
+                                buy.setBuyName(buyName);
+                                buy.setBuyCode(Integer.valueOf(splitSum[j]));
+                                buy.setBuyCount(buyCount);
+                                buyList.add(buy);
+                            }
+                        }
+                    }
+                }else if(StringUtils.isNoneBlank(splitLine) && splitLine.contains("/")) {
+                    //识别二：7/10 8/20 9/30
+                    String[] splitBlack = splitLine.split(" ");
+                    if(splitBlack != null && splitBlack.length >= 1){
+                        for(int j=0;j<splitBlack.length;j++){
+                            if(StringUtils.isNoneBlank(splitBlack[j]) && splitBlack[j].length() >= 3) {
+                                String[] splitOne = splitBlack[j].split("/");
+                                if(splitOne != null && splitOne.length == 2){
+                                    Buy buy = new Buy();
+                                    buy.setBuyId(buyId);
+                                    buy.setBuyName(buyName);
+                                    buy.setBuyCode(Integer.valueOf(splitOne[0]));
+                                    buy.setBuyCount(Integer.valueOf(splitOne[1]));
+                                    buyList.add(buy);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
